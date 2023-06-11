956e842791607412b4babc3722b71b68a27d87e5
ScribbleRiyad <scribbleriyad@gmail.com>
2023-06-11 17:16:21 +0600

    produect Cart Completed

---
+++ lib/ Widgets/product_card.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:online_shop/%20Widgets/appStyle.dart';
+
+class ProductCart extends StatefulWidget {
+  const ProductCart(
+      {super.key,
+      required this.id,
+      required this.category,
+      required this.image,
+      required this.name,
+      required this.price});
+
+  final String name;
+  final String price;
+  final String category;
+  final String id;
+  final String image;
+
+  @override
+  State<ProductCart> createState() => _ProductCartState();
+}
+
+class _ProductCartState extends State<ProductCart> {
+  bool selected = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(
+        8,
+        0,
+        20,
+        0,
+      ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(16)),
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width * 0.26,
+          decoration: const BoxDecoration(color: Colors.white, boxShadow: [
+            BoxShadow(
+                blurRadius: 06,
+                spreadRadius: 1,
+                color: Colors.white,
+                offset: Offset(1, 1))
+          ]),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.23,
+                    decoration: BoxDecoration(
+                        image:
+                            DecorationImage(image: NetworkImage(widget.image))),
+                  ),
+                  Positioned(
+                      right: 10.00,
+                      top: 10.00,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: const Icon(Icons.heart_broken),
+                      ))
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.name,
+                      style: appStyleWithHt(
+                          16, Colors.black, FontWeight.bold, 1.1),
+                    ),
+                    Text(
+                      widget.category,
+                      style:
+                          appStyleWithHt(16, Colors.grey, FontWeight.bold, 1.5),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.price,
+                      style: appStyleWithHt(
+                          16, Colors.black, FontWeight.bold, 1.1),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Colors",
+                          style: appStyleWithHt(
+                              16, Colors.grey, FontWeight.bold, 1.1),
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        ChoiceChip(
+                          label: const Text(""),
+                          selected: selected,
+                          visualDensity: VisualDensity.comfortable,
+                          selectedColor: Colors.black,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/View/Presentations/homepage.dart
+++ lib/View/Presentations/homepage.dart
@@ -1,5 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_antd_icons/flutter_antd_icons.dart';
 import 'package:online_shop/%20Widgets/appStyle.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,65 +15,12 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFE2E2E2),
       body: Center(
           child: SizedBox(
         height: MediaQuery.of(context).size.height,
         child: Stack(
           children: [
-            Padding(
-              padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.265),
-              child: TabBarView(controller: _tabController, children: [
-                Column(
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.45,
-                      color: Colors.amber,
-                    ),
-                    Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "New Collection",
-                              style:
-                                  appStyle(20, Colors.black, FontWeight.bold),
-                            ),
-                            Row(
-                              children: [
-                                Text(
-                                  "Show All",
-                                  style: appStyle(
-                                      20, Colors.black, FontWeight.bold),
-                                ),
-                                const Icon(AntdIcons.caret_right_filled)
-                              ],
-                            )
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.45,
-                      color: Colors.amber,
-                    )
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.45,
-                      color: Colors.amber,
-                    )
-                  ],
-                ),
-              ]),
-            ),
             Container(
               padding: const EdgeInsets.fromLTRB(16, 45, 0, 0),
               height: MediaQuery.of(context).size.height * 0.4,
@@ -114,7 +61,116 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                   ],
                 ),
               ),
-            )
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.25),
+              child: Container(
+                padding: const EdgeInsets.only(left: 12),
+                child: TabBarView(controller: _tabController, children: [
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.405,
+                        child: ListView.builder(
+                            itemCount: 6,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  color: Colors.grey,
+                                  height: MediaQuery.of(context).size.height,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.6,
+                                ),
+                              );
+                            }),
+                      ),
+                      Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(12, 10, 12, 10),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "New Collection",
+                                  style: appStyle(
+                                      16, Colors.black, FontWeight.bold),
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      "Show All",
+                                      style: appStyle(
+                                          16, Colors.black, FontWeight.bold),
+                                    ),
+                                    const Icon(
+                                      Icons.arrow_right,
+                                      size: 32,
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.13,
+                            child: ListView.builder(
+                                itemCount: 6,
+                                scrollDirection: Axis.horizontal,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Container(
+                                      decoration: const BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(16)),
+                                          boxShadow: [
+                                            BoxShadow(
+                                                color: Colors.black38,
+                                                blurRadius: 0.8,
+                                                offset: Offset(0, 1),
+                                                spreadRadius: 1),
+                                          ]),
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.12,
+                                      width: MediaQuery.of(context).size.width *
+                                          0.28,
+                                      child: CachedNetworkImage(
+                                        imageUrl:
+                                            "https://d326fntlu7tb1e.cloudfront.net/uploads/58282ea3-b815-4d26-9f4f-382aa62f67cf-HP5404_a1.webp",
+                                      ),
+                                    ),
+                                  );
+                                }),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.45,
+                        color: Colors.amber,
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.45,
+                        color: Colors.amber,
+                      )
+                    ],
+                  ),
+                ]),
+              ),
+            ),
           ],
         ),
       )),
